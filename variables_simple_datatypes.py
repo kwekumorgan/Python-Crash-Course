69d80b856bfcb9ecfaea053391226ffedc6195f0
kwekumorgan <morganmiguel.30@gmail.com>
2024-09-03 11:32:01 +0000

    worked on variables in strings and worked on  whitespaces using tabs and newlines

--- variables_simple_datatypes.py
+++ variables_simple_datatypes.py
@@ -11,3 +11,24 @@ print(name.title())
 print(name.upper())
 print(name.lower())
 
+# using variables in strings
+
+f_name="james"
+l_name="ward-prowse"
+
+print(f'{f_name.title()} {l_name.title()} is a footballer that plays in West Ham colors')
+
+info=f'{f_name.title()} {l_name.title()} is a footballer that plays in West Ham colors'
+
+print(info)
+
+#Adding Whitespace to Strings with Tabs or Newlines
+
+print(f'\t name.title()')
+
+print(f'Adjoa:The new King has been born\nKofi: Who is the the King?\nAdjoa: Jesus Christ ')
+
+print(f'Types of language:\n\tSpanish\n\tItalian\n\tFrench\n\t Dutch\n\tEnglish')
+
+
+
