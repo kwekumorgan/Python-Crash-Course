ee6a0a388fe82ee6c216552a143f680fd83778ad
kwekumorgan <morganmiguel.30@gmail.com>
2024-09-03 10:42:36 +0000

    started working with variables and srtings

---
+++ variables_simple_datatypes.py
@@ -0,0 +1,13 @@
+
+# Naming & Using Varibales 
+greetings= "Hello, goodmorning!"
+print(greetings)
+
+
+# Strings
+name="James Ward-Prowse"
+
+print(name.title())
+print(name.upper())
+print(name.lower())
+
