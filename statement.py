cb0924adec9998fdb461ab17a7bdca7fa1609a60
kwekumorgan <morganmiguel.30@gmail.com>
2024-09-11 23:32:43 +0000

    sample of statements

---
+++ statement.py
@@ -0,0 +1,12 @@
+#
+cars=["toyota supra","aventador","lambourghini","bmw m5","Range Rover"]
+
+for car in cars:
+    if car.lower()=="bmw m5":
+        print(car.upper())
+    else:
+        print(car.lower())
+
+
+# CONDITIONAL TESTS
+
