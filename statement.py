4ea081117bb3e0b4a805930da06d9b7c89526ab3
kwekumorgan <morganmiguel.30@gmail.com>
2024-09-13 22:03:53 +0000

    completed task 1 - statement

--- statement.py
+++ statement.py
@@ -10,3 +10,46 @@ for car in cars:
 
 # CONDITIONAL TESTS
 
+requested_toppings=["Onion","mushrooms","Pineapple"]
+ 
+
+
+
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+upper_car= car.upper()
+
+print("Is car == SUBARU? " )
+
+print(upper_car==car.lower())
+
+
+
+
+kwame_age=24
+afia_age=32
+
+if (kwame_age<=36) and (afia_age<40):
+    print("Both Afia and Kwame are in range ")
+
+
+name_person1="James ward "
+name_person2="Fifii Prat"
+
+
+if name_person1=="James ward " or name_person2=="jungle":
+    print("We found one of them ")
+
+
+if "Onion" in requested_toppings:
+    print("Confirmed Onion is part of the ingredient ")
+
+
+if "Banana" not in requested_toppings:
+    print("Banana is not in the ingredient")
