fbf71d69c71f583b4f17137dfd29305f78d85bf6
kwekumorgan <morganmiguel.30@gmail.com>
2024-09-14 22:51:36 +0000

    final task completed- statement

--- statement.py
+++ statement.py
@@ -153,3 +153,16 @@ for new_user in new_users:
     else:
         print(f"{new_user} is available")
         
+
+
+ordinal_numbers=[1,2,3,4,5,6,7,8,9]
+
+for ordinal_number in ordinal_numbers:
+    if (ordinal_number ==1):
+        print(f"{ordinal_number}st")
+    elif ordinal_number ==2:
+        print(f"{ordinal_number}nd")
+    elif ordinal_number==3:
+        print(f"{ordinal_number}rd")
+    elif ordinal_number > 3:
+        print(f"{ordinal_number}th")
