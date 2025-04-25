096102feeeb8569968addfefb7580ceefde687bc
kwekumorgan <morganmiguel.30@gmail.com>
2025-04-25 10:01:22 +0000

    try work on function with parameter

---
+++ functions.py
@@ -0,0 +1,18 @@
+#passing information into function 
+def greet_user(username):
+    print(f'hello,{username}')
+
+greet_user('kwame')
+
+
+#TRY WORK
+
+def display_message():
+    print("parameter is the variable in a defined function and argument is the value of the variable that is passed into function ")
+
+display_message()
+
+def favourite_book(title):
+    print(f"My favourite book is {title.title()}")
+
+favourite_book('Lion king')
