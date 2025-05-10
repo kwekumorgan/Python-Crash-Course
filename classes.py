7e79f4710bdf7b73462bd041b7e946cf6fe73c8c
kwekumorgan <morganmiguel.30@gmail.com>
2025-05-10 23:38:53 +0000

    completed task 1.1

--- classes.py
+++ classes.py
@@ -5,10 +5,10 @@ class Dog:
 
 
     def sit(self):
-        print(f'the{self.name} is sitting down')
+        print(f'{self.name} is sitting down')
 
     def lying(self ):
-        print(f'the {self.name} is lying down')
+        print(f' {self.name} is lying down')
 
 
 
@@ -17,3 +17,29 @@ my_dog= Dog('Jack',3)
 
 
 print(f'my name is {my_dog.name}')
+
+
+my_dog.sit()
+
+
+
+# try work
+
+
+class restaurant:
+    def __init__(self,restaurant_name,restaurant_cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.restaurant_cuisine_type = restaurant_cuisine_type
+
+    def describe_restaurant():
+        print('This restaurant is an Italian based restaurant')
+
+    def open_restaurant():
+        print('We open at 8Am everyday')
+
+
+
+
+my_restaurant= restaurant('La famille','Fastfood') 
+print(my_restaurant.restaurant_name)
+
